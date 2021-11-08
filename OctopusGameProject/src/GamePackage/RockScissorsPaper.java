c1ed12d970d47352d7e3c11d521598a46a8f26ca
yeonwoo1125 <s2021@e-mirim.hs.kr>
2021-11-08 14:57:52 +0900

    가위바위보 수정

--- OctopusGameProject/src/GamePackage/RockScissorsPaper.java
+++ OctopusGameProject/src/GamePackage/RockScissorsPaper.java
@@ -10,72 +10,66 @@ public class RockScissorsPaper {
 		 int ran;  
 		 String rpsList[]= {"가위","바위","보","가위","바위","보","가위","바위","보","가위","바위","보"};
 		 String user_sel="";
-		 String com_sel;
+		 String master_sel;
 		 int turn=0;
-		 int lose_cnt=0;
+		 int win_cnt=0;
 		 while(turn<3) {
 			 turn++;
 			 ran = rand.nextInt(12);  
+			 System.out.println("세판 중 두판을 이기거나 비기면 선공, 두판을 지면 후공입니다.");
 			 System.out.println("가위 바위 보 중 하나를 골라주세요");
-			 System.out.println("exit�� ������ ����");
+			 
 			 user_sel=sc.next();
-			 com_sel=rpsList[ran];
-			 if(user_sel.equals("exit")) break;
-			 System.out.println("���� �� �� : "+com_sel);
-			 if(user_sel.equals("����")) {
-				 if(user_sel.equals(com_sel)) {
-					 System.out.println("�����ϴ�.");
+			 master_sel=rpsList[ran];
+			 System.out.println("상대방 : "+master_sel);
+			 if(user_sel.equals("가위")) {
+				 if(user_sel.equals(master_sel)) {
+					System.out.println("비겼습니다.");
 					 
 				 }
-				 else if(com_sel.equals("��")){
-					 System.out.println("�̰���ϴ�.");
+				 else if(master_sel.equals("보")){
+					System.out.println("이겼습니다.");
+					win_cnt++;
 				 }
 				 else {
-					 System.out.println("�����ϴ�.");
-					 lose_cnt++;
-					 if(lose_cnt==2) {
-						 System.out.println("�׾���");
-						 return false;
-					 } 
+					 System.out.println("졌습니다.");
 				 }
 			 }
-			 else if(user_sel.equals("����")) {
-				 if(user_sel.equals(com_sel)) {
-					 System.out.println("�����ϴ�.");
+			 else if(user_sel.equals("바위")) {
+				 if(user_sel.equals(master_sel)) {
+					System.out.println("비겼습니다.");
 				 }
-				 else if(com_sel.equals("����")){
-					 System.out.println("�̰���ϴ�.");
+				 else if(master_sel.equals("가위")){
+					System.out.println("이겼습니다.");
+					win_cnt++;
 				 }
 				 else {
-					 System.out.println("�����ϴ�.");
-					 lose_cnt++;
-					 if(lose_cnt==2) {
-						 System.out.println("�׾���");
-						 return false;
-					 } 
+					 System.out.println("졌습니다.");
 				 }
 			 }
-			 else if(user_sel.equals("��")) {
-				 if(user_sel.equals(com_sel)) {
-					 System.out.println("�����ϴ�.");
+			 else if(user_sel.equals("보")) {
+				 if(user_sel.equals(master_sel)) {
+					System.out.println("비겼습니다.");
 				 }
-				 else if(com_sel.equals("����")){
-					 System.out.println("�̰���ϴ�.");
+				 else if(master_sel.equals("바위")){
+					System.out.println("이겼습니다.");
+					win_cnt++;
 				 }
 				 else {
-					 System.out.println("�����ϴ�.");
-					 lose_cnt++;
-					 if(lose_cnt==2) {
-						 System.out.println("�׾���");
-						 return false;
-					 } 
+					 System.out.println("졌습니다.");
 				 }
 			 }
 			 System.out.println();
 		 }
-		 System.out.println();
-		 System.out.println("���");
-		 return true;
+		 if(win_cnt == 2){
+			System.out.println("당신은 선공입니다.");
+			return true;
+		 }
+		 else {
+			 System.out.println("당신은 후공입니다");
+			 return false;
+		 }
+		 
 	}
 
 }
