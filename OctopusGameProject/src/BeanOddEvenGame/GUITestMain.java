3a6a8ca47cc9d818d217a5321a516ecb02d0cafb
SRin23 <serin4723@naver.com>
2021-11-16 16:12:27 +0900

    check commit(serin)

--- OctopusGameProject/src/BeanOddEvenGame/GUITestMain.java
+++ OctopusGameProject/src/BeanOddEvenGame/GUITestMain.java
@@ -1,16 +1,14 @@
 package BeanOddEvenGame;
 
-import RPS.rps_gui;
-
 public class GUITestMain {
 
 	public static void main(String[] args) {
-		//defenceBeaningGameGUI bg = new defenceBeaningGameGUI();
-        //bg.beaningFrame(456, 3);
+		defenceBeaningGameGUI bg = new defenceBeaningGameGUI();
+        bg.beaningFrame(456, 3);
 		
 		
-		attackBeaningGameGUI ag = new attackBeaningGameGUI();
-		ag.beaningFrame(456, 3);
+		//attackBeaningGameGUI ag = new attackBeaningGameGUI();
+		//ag.beaningFrame(456, 3);
 	}
 
 }
--- OctopusGameProject/src/BeanOddEvenGame/attackBeaningGameGUI.java
+++ OctopusGameProject/src/BeanOddEvenGame/attackBeaningGameGUI.java
@@ -9,8 +9,6 @@ public class attackBeaningGameGUI extends JFrame{
 	void beaningFrame(int userNo, int heart) {
 		JFrame bf = new JFrame("±¸½½Ä¡±â(È¦Â¦)");
 		bf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
 	}
 }
 
