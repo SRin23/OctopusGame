c39481dcc851160d445bb2ae315b706e415791ef
yeonwoo1125 <s2021@e-mirim.hs.kr>
2021-12-02 01:20:43 +0900

    턴 수 제한 및 기타 수정

--- OctopusGameProject/src/RPS/rps_test.java
+++ OctopusGameProject/src/RPS/rps_test.java
@@ -2,9 +2,6 @@ package RPS;
 
 public class rps_test {
    public static void main(String[] args) {
-        //rpsGame rr = new rpsGame("456","457");
-        //rr.onclick();
-	    rps r = new rps();
-	    System.out.println("Ä¿¹ÔÅ×½ºÆ®");
+	   new rps();
     }
 }
