b35a116f1ffa42cd03fb27cfad08613744d561cd
yeonwoo1125 <s2021@e-mirim.hs.kr>
2021-12-03 21:52:44 +0900

    객체 생성 수정

--- OctopusGameProject/src/mainMenu/main_test.java
+++ OctopusGameProject/src/mainMenu/main_test.java
@@ -3,8 +3,7 @@ package mainMenu;
 public class main_test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		main_gui mg = new main_gui();
+		new main_gui();
 
 	}
 
