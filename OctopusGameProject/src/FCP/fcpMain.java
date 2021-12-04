151cb36eb2b8652a66a816ad0d2b92e627594f95
yeonwoo1125 <s2021@e-mirim.hs.kr>
2021-12-04 16:29:42 +0900

    내용 수정

--- OctopusGameProject/src/FCP/fcpMain.java
+++ OctopusGameProject/src/FCP/fcpMain.java
@@ -7,29 +7,9 @@ public class fcpMain {
 	static int countDown = 0;
 	static boolean win;
 	public static void main(String[] args) {
-		int heart=3;
 		fcp fg = new fcp("123");
+		fg.startGame();
 
-		//3초 뒤 게임 시작
-		Timer t = new Timer();
-		TimerTask tt = new TimerTask() {
-			
-			@Override
-			public void run() {
-				if(countDown<3) {
-					System.out.println(3-countDown+"...");
-					countDown++;
-				}
-				else {
-					t.cancel();
-					System.out.println("게임 스타트");
-					win = fg.startGame();
-				}
-			}
-		};
-		t.scheduleAtFixedRate(tt, 1000,1000);
-		
-		if(!win) heart--; 
 	}
 	
 }
