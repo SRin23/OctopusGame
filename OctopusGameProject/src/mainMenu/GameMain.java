6314b831975f18f469c94c99af54de40e201308a
SRin23 <serin4723@naver.com>
2021-12-07 23:04:39 +0900

    Merge 정리

--- OctopusGameProject/src/mainMenu/GameMain.java
+++ OctopusGameProject/src/mainMenu/GameMain.java
@@ -1,10 +1,7 @@
 package mainMenu;
 
-<<<<<<< HEAD
 //가장 처음 시작하는 파일입니다.
 
-=======
->>>>>>> branch 'main' of https://github.com/SRin23/OctopusGame.git
 public class GameMain {
 	public static final int SCREEN_WIDTH = 1200;
 	public static final int SCREEN_HEIGHT = 900;
